6258b8be6944f067b389f0a1e434d5921668ba29
Wisnu Agastya <wisnu.aga@gmail.com>
2025-08-25 14:54:14 +0700

    init schemas

---
+++ schemas/validation.py
@@ -0,0 +1,29 @@
+
+from pydantic import BaseModel
+from typing import Any, List, Optional, Literal
+
+
+class Deviation(BaseModel):
+    field: str
+    pdf_value: Optional[Any]
+    djp_api_value: Optional[Any]
+    deviation_type: Literal["mismatch","missing_in_pdf","missing_in_api"]
+
+class ValidatedData(BaseModel):
+    npwpPenjual: Optional[str] = None
+    namaPenjual: Optional[str] = None
+    npwpPembeli: Optional[str] = None
+    namaPembeli: Optional[str] = None
+    nomorFaktur: Optional[str] = None
+    tanggalFaktur: Optional[str] = None
+    jumlahDpp: Optional[str] = None
+    jumlahPpn: Optional[str] = None
+
+class ValidationResults(BaseModel):
+    deviations: List[Deviation]
+    validated_data: ValidatedData
+
+class ValidationResponse(BaseModel):
+    status: Literal["validated_with_deviations", "validated_successfully", "error"]
+    message: str
+    validation_results: ValidationResults
